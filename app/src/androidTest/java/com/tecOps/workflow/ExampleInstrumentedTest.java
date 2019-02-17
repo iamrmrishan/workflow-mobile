1337f4357cd721a8f9c91eb82e41173b42df290a
Ahmed Ifhaam <ahmedifhaam1@gmail.com>
2019-02-17 16:31:08 +0530

    Updated the test case - since travis build failed

--- app/src/androidTest/java/com/tecOps/workflow/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tecOps/workflow/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.kalpa.workflow", appContext.getPackageName());
+        assertEquals("com.tecOps.workflow", appContext.getPackageName());
     }
 }
